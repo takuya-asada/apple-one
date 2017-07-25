c56dbc7e0f51b2d38951fb2be29b18773e507c91
Takuya Asada <dirigo.takuya@gmail.com>
2017-07-25 11:59:25 +0900

    Initial Commit

---
+++ apple-one/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apple-one
+//
+//  Created by 浅田拓哉 on 2017/07/25.
+//  Copyright © 2017年 浅田拓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
