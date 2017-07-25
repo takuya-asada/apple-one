14100395b7333fec92e386630333d48a4eab66e3
Takuya Asada <dirigo.takuya@gmail.com>
2017-07-25 17:52:26 +0900

    2017/07/25/1752

--- apple-one/ViewController.swift
+++ apple-one/ViewController.swift
@@ -7,8 +7,38 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    func getArticle(){
+        Alamofire.request("https://qiita.com/api/v2/items",method:.get, parameters:nil)
+            .responseJSON { response in
+                print(response.result.value as Any) // responseのresultプロパティのvalueプロパティをコンソールに出力
+        }
+    }
+    
+    @IBAction func button(_ sender: Any) {
+    
+        getArticle()
+        
+        do{
+            let toJSON: JSON =  ["name": "Jack", "age": 25]
+            textView.text = "\(toJSON)"
+            
+            
+            let jsonString = "{\"あああ\": 25}"
+            let dataFromString = jsonString.data(using: String.Encoding.utf8)
+            var json = try JSON(data: dataFromString!)
+            print(json["あああ"])
+        }catch{
+            print(error)
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
